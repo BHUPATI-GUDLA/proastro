5a0bce60e1e535162a53652b453f62a84c338c7b
bgudla <bhupatigudla00@gmail.com>
2024-02-25 03:14:32 +0530

    Initial setup

---
+++ proastro/src/main/java/com/proastro/gua/ProastroApplication.java
@@ -0,0 +1,14 @@
+package com.proastro.gua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProastroApplication.class, args);
+		System.out.println("Started my first spring boot project...");
+	}
+
+}
---
+++ proastro/src/test/java/com/proastro/gua/ProastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proastro.gua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
