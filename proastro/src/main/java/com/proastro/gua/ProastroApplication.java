8a3478f44816d49c4e757db92aa4e6fcbcfa0a1b
bgudla <bgudla@localhost>
2024-03-19 00:32:12 +0530

    Email Configuration Changes

--- proastro/src/main/java/com/proastro/gua/ProastroApplication.java
+++ proastro/src/main/java/com/proastro/gua/ProastroApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProastroApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProastroApplication.class, args);
 		System.out.println("Started my first spring boot project...");
+	
 	}
 
 }
--- proastro/src/test/java/com/proastro/gua/ProastroApplicationTests.java
+++ proastro/src/test/java/com/proastro/gua/ProastroApplicationTests.java
@@ -1,13 +1,26 @@
 package com.proastro.gua;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.proastro.gua.mail.EmailService;
 
 @SpringBootTest
 class ProastroApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Autowired
+	private EmailService emailService;
+	
+	@Test
+	void testsendMail() {
+		System.out.println("Running inside test method....");
+		emailService.sendEmail("kevalborker@gmail.com", "Dummy mail from Bhupati", "Hello saroja this mail i am sending to you by my java project. whenever you see this mail just call me and tell i recived your mail.");
+		System.out.println("Mail Send successfully...");
 	}
 
 }
